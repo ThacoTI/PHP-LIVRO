9316a8a117058bbb52917713fcf7b54a08e1864c
Antônio Mateus de Sousa Silveira <113131148+ThacoTI@users.noreply.github.com>
2023-09-25 23:35:45 -0300

    Estudo, nao funcionor

---
+++ estudo/processando.php
@@ -0,0 +1,18 @@
+
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seus Resutados </title>
+</head>
+<body>
+    <h1>Bobs Autos</h1>
+    <h2  <?php
+        echo '<p>orde proseco</p>';
+    ?>>
+   
+    resutados </h2>
+    
+    
+</body>
+</html>
